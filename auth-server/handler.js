ad48c02d98430312e07e5d0fc09bb0e48c5e680d
patrickderrickson <90005529+patrickderrickson@users.noreply.github.com>
2022-04-07 09:06:58 -0700

    added new origin

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -20,7 +20,7 @@ const credentials = {
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://patrickderrickson.github.io/meet-app/"],
-  javascript_origins: ["https://patrickderrickson.github.io", "http://localhost:3000"],
+  javascript_origins: ["https://patrickderrickson.github.io", "http://localhost:3000", "http://127.0.0.1:8080"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
