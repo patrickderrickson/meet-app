70a8eec2d03bd12564ccf23fb7609e06962cbed7
patrickderrickson <90005529+patrickderrickson@users.noreply.github.com>
2022-04-19 16:06:01 -0700

    added api

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 import { mockData } from './mock-data';
 import axios from 'axios';
+import NProgress from 'nprogress';
 
 
 export const extractLocations = (events) => {
@@ -16,6 +17,35 @@ export const extractLocations = (events) => {
       .catch((error) => error.json());
   
     return result;
+  };
+
+  const getToken = async (code) => {
+    const encodeCode = encodeURIComponent(code);
+    const { access_token } = await fetch(
+      'https://ddyncsywl8.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .catch((error) => error);
+  
+    access_token && localStorage.setItem("access_token", access_token);
+  
+    return access_token;
+  };
+
+  const removeQuery = () => {
+    if (window.history.pushState && window.location.pathname) {
+      var newurl =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        window.location.pathname;
+      window.history.pushState("", "", newurl);
+    } else {
+      newurl = window.location.protocol + "//" + window.location.host;
+      window.history.pushState("", "", newurl);
+    }
   };
 
   export const getEvents = async () => {
